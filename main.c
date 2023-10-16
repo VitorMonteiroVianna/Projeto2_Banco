5a99d94848dd5d2ef7dedda59a42ac78ad0010e6
HG0304 <105385233+HG0304@users.noreply.github.com>
2023-10-16 19:56:59 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,54 +2,54 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main( )
-{
-    int(operador);
+int main() {
+  int(operador);
 
-    printf("BANCO QUEM POUPA TEM\n\n");
-    printf("Digite o numero da opcao desejada:\n");
-    printf("1 -> Novo cliente\n");
-    printf("2 -> Apagar cliente\n");
-    printf("3 -> Listar clientes\n");
-    printf("4 -> Efetuar um debito\n");
-    printf("5 -> Efetuar um deposito\n");
-    printf("6 -> Exibir extrato\n");
-    printf("7 -> Tranferencia entre contas\n");
-    printf("0 -> Sair\n");
+  printf("BANCO QUEM POUPA TEM\n\n");
+  printf("Digite o numero da opcao desejada:\n");
+  printf("1 -> Novo cliente\n");
+  printf("2 -> Apagar cliente\n");
+  printf("3 -> Listar clientes\n");
+  printf("4 -> Efetuar um debito\n");
+  printf("5 -> Efetuar um deposito\n");
+  printf("6 -> Exibir extrato\n");
+  printf("7 -> Tranferencia entre contas\n");
+  printf("0 -> Sair\n");
 
-    scanf("%d", &operador);
+  scanf("%d", &operador);
 
-    switch(operador) {
-        case 1:
-            //Aqui vem a funcao de adicionar novo cliente
-            break;
-        case 2:
-            //Aqui vem a funcao de apagar cliente
-            break;
-        case 3:
-            //Aqui vem a funcao de Listar clientes
-            break;
-        case 4:
-            //Aqui vem a funcao de efetuar debito
-            break;
-        case 5:
-            //Aqui vem a funcao de efetuar deposito
-            break;
-        case 6:
-            //Aqui vem a funcao de exibir o extrato
-            break;
-        case 7:
-            //Aqui vem a funcao de tranferencia entre contas
-            break;
-        case 0:
-            printf("Saindo!\n");
-            sleep(1);
-            break;
-        default:
-            printf("Digite uma opcao VALIDA!\n");
-            break;
-    }
+  switch (operador) {
+  case 1:
+    novo_cliente();
+    break;
+  case 2:
+    // Aqui vem a funcao de apagar cliente
+    break;
+  case 3:
+    // Aqui vem a funcao de Listar clientes
+    break;
+  case 4:
+    // Aqui vem a funcao de efetuar debito
+    break;
+  case 5:
+    // Aqui vem a funcao de efetuar deposito
+    break;
+  case 6:
+    // Aqui vem a funcao de exibir o extrato
+    break;
+  case 7:
+    // Aqui vem a funcao de tranferencia entre contas
+    break;
+  case 0:
+    printf("Saindo!\n");
+    sleep(1);
+    break;
+  default:
+    printf("Digite uma opcao VALIDA!\n");
+    break;
+  }
 
-    while(operador != 0);
-    return 0;
+  while (operador != 0)
+    ;
+  return 0;
 }
